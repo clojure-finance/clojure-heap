69b10a3e4e9e26a2d98002dbf957795ccf5c1000
jiarui0000 <jiaruiz@connect.hku.hk>
2022-02-22 09:01:10 +0800

    build heap based on priority queue

---
+++ heap_from_java/test/heap_from_java/core_test.clj
@@ -0,0 +1,7 @@
+(ns heap-from-java.core-test
+  (:require [clojure.test :refer :all]
+            [heap-from-java.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
